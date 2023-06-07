f7150822a2aa8961bdad73af1d528f0428b4ccdf
Flomelli <114627202+Flomelli@users.noreply.github.com>
2023-06-07 12:33:44 -0500

    heart class change

--- script/index.js
+++ script/index.js
@@ -4,8 +4,8 @@ let heartImg = document.querySelector('.heart');
 function toggleLike() {
     liked = !liked;
     if (liked) {
-        heartImg.src = './images/filledheart.png';
+        heart.src = './images/filledheart.png';
     } else {
-        heartImg.src = './images/heart.png';
+        heart.src = './images/heart.png';
     }
 }
